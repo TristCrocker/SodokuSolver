4370eb0446da4444d408cea508798e6701cdac2a
Tristan Crocker <tristycrocks@gmail.com>
2020-03-30 09:56:12 +0200

    Add files via upload

--- SodokuSolver.py
+++ SodokuSolver.py
@@ -2,16 +2,9 @@
 import numpy as np
 import random
 
-board = np.array([[3, 0, 6, 5, 0, 8, 4, 0, 0],[5, 2, 0, 0, 0, 0, 0, 0, 0],[0, 8, 7, 0, 0, 0, 0, 3, 1],[0, 0, 3, 0, 1, 0, 0, 8, 0],[9, 0, 0, 8, 6, 3, 0, 0, 5],[0, 5, 0, 0, 9, 0, 6, 0, 0],[1, 3, 0, 0, 0, 0, 2, 5, 0],[0, 0, 0, 0, 0, 0, 0, 7, 4],[0, 0, 5, 2, 0, 6, 3, 0, 0]]).reshape(9,9)
+# board = np.array([[3, 0, 6, 5, 0, 8, 4, 0, 0],[5, 2, 0, 0, 0, 0, 0, 0, 0],[0, 8, 7, 0, 0, 0, 0, 3, 1],[0, 0, 3, 0, 1, 0, 0, 8, 0],[9, 0, 0, 8, 6, 3, 0, 0, 5],[0, 5, 0, 0, 9, 0, 6, 0, 0],[1, 3, 0, 0, 0, 0, 2, 5, 0],[0, 0, 0, 0, 0, 0, 0, 7, 4],[0, 0, 5, 2, 0, 6, 3, 0, 0]]).reshape(9,9)
 # board = np.array([[3,0,0],[0,0,0],[0,2,0]]).reshape(3,3)
 
-def check_empty(board):
-    #Check for unused blocks
-    for row in range(board.shape[0]):
-        for col in range(board.shape[1]):
-            if board[row][col] == 0:
-                return (row,col)
-    return None
 
 def check_valid(board, value, index):
 
@@ -88,13 +81,25 @@ def check_valid(board, value, index):
     return True
 
 def solve(board):
-    index = check_empty(board)
-    if index == None:
+    #Check for empty blocks
+    empty = False
+
+    for row in range(board.shape[0]):
+        if empty == True:
+            break
+        for col in range(board.shape[1]):
+            if board[row][col] == 0:
+                empty = True
+                index = [row,col]
+                break
+
+    if empty == False:
         '''This check if there are any more blocks, in the lowest level solve(board) condition, 
            on the last solution if a number picked is valid then it'll enter into a new solve(board) condition, 
            but index will be none so it'll return true, causing all upper solve(board) conditions to return true until the outter solve(board),
            therefore all conditions will be true and it'll be solved'''
         return True
+
     for n in range(1, board.shape[0] + 1):
         if check_valid(board, n, index):
             board[index[0]][index[1]] = n
